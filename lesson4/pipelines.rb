c28e6274f9d4e856b74055a2316f6703093c5610
Terrence Cole <terrence.cole@procore.com>
2019-05-16 08:53:39 -0700

    Add the example code we are trying to make work.

---
+++ lesson4/pipelines.rb
@@ -0,0 +1,14 @@
+puts Integer::sqrt(2)
+puts 2 |> Integer::sqrt
+
+# def my_map(arr, &block)
+#   arr.collect(&block)
+# end
+
+# def my_sum(arr)
+#   arr.sum
+# end
+
+# test = ["foo", "bar", "baz"]
+# puts my_sum(my_map(test, &:length))
+# puts test |> my_map(&:length) |> my_sum
